67844f8fcdb3781eaf64b1300db6fdf4a4414155
lucijs <fradelic.lucija@gmail.com>
2024-04-05 23:01:50 +0200

    Added pages for log in and registration

---
+++ frontend/src/pages/Authentication/LogIn/LogIn.jsx
@@ -0,0 +1,11 @@
+import classes from "./index.module.css";
+
+const LogIn = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ frontend/src/pages/Authentication/Register/Register.jsx
@@ -0,0 +1,11 @@
+import classes from "./index.module.css";
+
+const Register = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Register;
