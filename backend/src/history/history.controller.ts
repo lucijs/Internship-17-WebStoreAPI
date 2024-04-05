f0a6e471c0c050bafe6d3a4b17fe7fded56f9dde
lucijs <fradelic.lucija@gmail.com>
2024-04-05 21:24:02 +0200

    Adding AuthGuards to history

--- backend/src/history/history.controller.ts
+++ backend/src/history/history.controller.ts
@@ -1,39 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { HistoryService } from './history.service';
 import { CreateHistoryDto } from './dto/create-history.dto';
 import { UpdateHistoryDto } from './dto/update-history.dto';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { HistoryEntity } from './entities/history.entity';
+import { UserAuthGuard } from 'src/users/user-auth.guard';
+import { AdminAuthGuard } from 'src/users/admin-auth.guard';
 
 @Controller('history')
 @ApiTags('History')
 export class HistoryController {
   constructor(private readonly historyService: HistoryService) {}
 
+  @UseGuards(UserAuthGuard)
   @Post()
   @ApiCreatedResponse({type: HistoryEntity})
-  create(@Body() createHistoryDto: CreateHistoryDto) {
-    return this.historyService.create(createHistoryDto);
+  create(@Req() {user},@Body() createHistoryDto: CreateHistoryDto) {
+    return this.historyService.create({...createHistoryDto, userId:user.id});
   }
 
+  @UseGuards(AdminAuthGuard)
   @Get()
   @ApiOkResponse({type: HistoryEntity, isArray:true})
   findAll() {
     return this.historyService.findAll();
   }
 
+  @UseGuards(UserAuthGuard)
   @Get(':id')
   @ApiOkResponse({type: HistoryEntity})
   findOne(@Param('id') id: string) {
     return this.historyService.findOne(+id);
   }
 
+  @UseGuards(AdminAuthGuard)
   @Patch(':id')
   @ApiOkResponse({type: HistoryEntity})
   update(@Param('id') id: string, @Body() updateHistoryDto: UpdateHistoryDto) {
     return this.historyService.update(+id, updateHistoryDto);
   }
 
+  @UseGuards(AdminAuthGuard)
   @Delete(':id')
   @ApiOkResponse({type: HistoryEntity})
   remove(@Param('id') id: string) {
