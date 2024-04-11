8d25bf1ea458c0cbaf05d28d64f4bef3b2ac15bc
lucijs <fradelic.lucija@gmail.com>
2024-04-11 03:22:59 +0200

    fixing imports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,14 +10,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle("Web Store")
-    .setDescription("API containing all information for web store")
-    .setVersion("0.1")
+    .setTitle('Web Store')
+    .setDescription('API containing all information for web store')
+    .setVersion('0.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api',app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
